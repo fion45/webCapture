10af5beddccea5d5a9e20eb20f477319b3408b14
fion45 <86945494@qq.com>
2014-05-19 18:06:26 +0800

    a

--- DBScript/insertValue.sql
+++ DBScript/insertValue.sql
@@ -29,6 +29,8 @@ insert [FCStore.Models.FCStoreDbContext].[dbo].[ColumnProducts] Values(1,68);
 insert [FCStore.Models.FCStoreDbContext].[dbo].[ColumnProducts] Values(1,69);
 
 insert [FCStore.Models.FCStoreDbContext].[dbo].[Roles] Values('admin','10000','admin','ALL');
+insert [FCStore.Models.FCStoreDbContext].[dbo].[Roles] Values('saler','500','saler','ALL');
+insert [FCStore.Models.FCStoreDbContext].[dbo].[Roles] Values('client','100','client','ALL');
 
 insert [FCStore.Models.FCStoreDbContext].[dbo].[Users] Values('fion','1','1','test@qq.com','');
 insert [FCStore.Models.FCStoreDbContext].[dbo].[Users] Values('test','2','2','test@qq.com','');
