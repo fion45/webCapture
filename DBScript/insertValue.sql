893bb62c5201367b3cf3ba9db675cf9a19ed1c45
fionhuo <86945494@qq.com>
2014-11-13 00:52:49 +0800

    1111111111

--- DBScript/insertValue.sql
+++ DBScript/insertValue.sql
@@ -70,9 +70,9 @@ insert [FCStoreWeb].[dbo].[Roles] Values('admin','10000','admin','ALL');
 insert [FCStoreWeb].[dbo].[Roles] Values('saler','500','saler','ALL');
 insert [FCStoreWeb].[dbo].[Roles] Values('client','100','client','ALL');
 
-insert [FCStoreWeb].[dbo].[Users] Values('fion','fion','12348765','86945494@qq.com',0,NULL,'',100,NULL,NULL,NULL,NULL);
-insert [FCStoreWeb].[dbo].[Users] Values('fion','1','111111','86945494@qq.com',0,NULL,'',100,NULL,NULL,NULL,NULL);
-insert [FCStoreWeb].[dbo].[Users] Values('test','2','222222','test@qq.com',0,NULL,'',100,NULL,NULL,NULL,NULL);
+insert [FCStoreWeb].[dbo].[Users] Values('fion','fion','12348765','86945494@qq.com',0,NULL,'',100,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert [FCStoreWeb].[dbo].[Users] Values('fion','1','111111','86945494@qq.com',0,NULL,'',100,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert [FCStoreWeb].[dbo].[Users] Values('test','2','222222','test@qq.com',0,NULL,'',100,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
 insert [FCStoreWeb].[dbo].[RoleUsers] Values(1,1);
 insert [FCStoreWeb].[dbo].[RoleUsers] Values(1,2);
